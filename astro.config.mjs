b6a0b9408bbcf966621ff3d84c8baa8816f9223c
yavko <yavornkolev@gmail.com>
2024-09-07 11:40:50 -0700

    chore: replace equiv deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,18 @@
 import { defineConfig } from 'astro/config';
 import compress from "astro-compress";
-import critters from "astro-critters";
 import sitemap from "astro-sitemap";
+import playformInline from "@playform/inline";
+import playformCompress from "@playform/compress";
+import playformFormat from "@playform/format";
 
 const site = 'https://troywebdev.com';
 // https://astro.build/config
 export default defineConfig({
   site,
-  integrations: [sitemap({xslUrl: site + '/assets/xsl/sitemap.xsl'}), critters(), compress()]
+  integrations: [
+    sitemap({xslUrl: site + '/assets/xsl/sitemap.xsl'}),
+    playformInline(),
+    playformCompress(),
+    playformFormat()
+  ]
 });
